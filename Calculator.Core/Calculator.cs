625bc3e22ecad61baec50204bd7dd896c41ce3e3
sarayu-kethidi <151454011+sarayu-kethidi@users.noreply.github.com>
2025-02-09 00:16:03 +0000

    Updated Code

--- Calculator.Core/Calculator.cs
+++ Calculator.Core/Calculator.cs
@@ -1,11 +1,43 @@
-﻿
-
-
-
-
-
-
-
+namespace Calculator.Core
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            checked
+            {
+                return a + b;
+            }
+        }
+        public static double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Divide by 0");
+            }
+            return (double)a / (double)b;
+        }
+        public static float Multiply(int a, float b)
+        {
+            checked
+            {
+                float result = a * b;
+                if (result > float.MaxValue || result < float.MinValue)
+                {
+                    throw new OverflowException("Multiplication result is out of range.");
+                }
+                return result;
+            }
+        }
+        public static int Subtract(int a, int b)
+        {
+            checked
+            {
+                return a - b;
+            }
+        }
+    }
+}
 
 
 
